dcf8eb1d76aecb1f84bdfa9f3d7f5c5d495a6dd1
MeDeity <langrenbule@gmail.com>
2018-10-11 23:26:39 +0800

    init commit

---
+++ src/main/java/com/deity/liveroom/IndexController.java
@@ -0,0 +1,19 @@
+package com.deity.liveroom;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+
+    @RequestMapping(value = "/index")
+    public String index(Model model){
+        return "index";
+    }
+
+    @RequestMapping(value = "/main")
+    public String main(){
+        return "/page/main";
+    }
+}
---
+++ src/main/java/com/deity/liveroom/LiveroomApplication.java
@@ -0,0 +1,12 @@
+package com.deity.liveroom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiveroomApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LiveroomApplication.class, args);
+    }
+}
